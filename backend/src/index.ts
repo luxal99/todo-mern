25e0995776a168e4dbf0e0e1655a2aa85fbd5920
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2020-12-13 17:07:16 +0100

    Init project

---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+import express = require("express");
+import "reflect-metadata";
+import {Request, Response} from "express";
+import bodyParser = require("body-parser");
+
+const cors = require('cors');
+const app = express();
