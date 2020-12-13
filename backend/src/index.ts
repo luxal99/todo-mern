32f09e02cdfd097427244f5d68c8395dc7a7a9dc
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2020-12-13 21:49:25 +0100

    Install typorm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,5 @@
 import express = require("express");
 import "reflect-metadata";
-import {Request, Response} from "express";
-import bodyParser = require("body-parser");
 
 const cors = require('cors');
 const app = express();
